bfe61a89258f46650b6acc56dd6eb54fd49b118b
IllyaShparuk <illya.shparuk@knu.ua>
2024-02-15 21:25:39 +0200

    Foundation for racing game
    Додав частину управління машиною, але є ще над чим працювати.

--- Project/main.py
+++ Project/main.py
@@ -6,26 +6,57 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((588,800))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("екн ерші")
-background_color = (255,255,255)
+background_color = (50, 50, 30)
 
-def get_car_image(filename,size,angle):
+
+def get_car_image(filename, size, angle):
     image = pygame.image.load(filename)
-    image = pygame.transform.scale(image,size)
-    image = pygame.transform.rotate(image,angle)
+    image = pygame.transform.scale(image, size)
+    image = pygame.transform.rotate(image, angle)
     return image
 
-my_car_image = get_car_image('images/car1.png',(50,50), 90)
 
-my_car = MyCar((250,600),my_car_image)
+my_car_image = get_car_image('Project/images/car1.png', (50, 50), 0)
+
+my_car = MyCar((800, 600), my_car_image, 0.1, 2)
 running = True
+angle = 0
+pos = [400, 300]
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    keys = pygame.key.get_pressed()
+    # if keys[pygame.K_LEFT]:
+    #     if my_car.speed <= my_car.max_speed:
+    #         my_car.speed += my_car.acceleration
+    #     my_car.move(-my_car.speed, 0)
+    # if keys[pygame.K_RIGHT]:
+    #     my_car.move(my_car.speed, 0)
+    # if keys[pygame.K_UP]:
+    #     if my_car.speed <= my_car.max_speed:
+    #         my_car.speed += my_car.acceleration
+    #     my_car.move(0, -my_car.speed)
+    # if keys[pygame.K_DOWN]:
+    #     if my_car.speed <= my_car.max_speed:
+    #         my_car.speed += my_car.acceleration
+    #     my_car.move(0, my_car.speed)
+
+    if keys[pygame.K_UP]:
+        if my_car.speed <= my_car.max_speed:
+            my_car.speed += my_car.acceleration
+        my_car.move()
+        if keys[pygame.K_LEFT]:
+            my_car.rotate(5)
+        if keys[pygame.K_RIGHT]:
+            my_car.rotate(-5)
+
+
+
     screen.fill(background_color)
     my_car.draw(screen)
     pygame.display.flip()
     clock.tick(60)
-
--- Project/my_car.py
+++ Project/my_car.py
@@ -1,7 +1,49 @@
+import math
+
+import pygame
+
+
 class MyCar:
-    def __init__(self,position, image):
+    def __init__(self, position, image, acceleration, max_speed):
         self.image = image
-        self.rect = self.image.get_rect
+        self.rect = self.image.get_rect()
         self.rect.center = position
+        self.max_speed = max_speed
+        self.speed = 0
+        self.angle = -90
+        self.acceleration = acceleration
+        self.vector = pygame.Vector2(0, self.image.get_width() / 2)
+        self.base_vector = pygame.Vector2(self.image.get_width() / 2, 0)
+        self.r = math.sqrt(
+            (position[0] - (position[0] + self.vector.x)) ** 2 + (position[1] - (position[1] + self.vector.y)) ** 2)
+
+    def move(self):
+        self.vector.x = self.r * math.cos(self.angle * math.pi / 180)
+        self.vector.y = self.r * math.sin(self.angle * math.pi / 180)
+        self.rect.x += self.vector.x * 0.1 * self.speed
+        self.rect.y += self.vector.y * 0.1 * self.speed
+
+    def rotate(self, y):
+        self.angle -= y
+        if self.angle > 180:
+            self.angle -= 360
+        if self.angle < -180:
+            self.angle += 360
+
     def draw(self, screen):
+        angle = self.__calculate_angle()
+        diff = round(abs(self.angle)-abs(angle), 1)
+        # if diff != 0:
+        #     self.image = pygame.transform.rotate(self.image, diff)
+        #     self.rect = self.image.get_rect(center=self.image.get_rect(center=self.rect.center).center)
         screen.blit(self.image, self.rect)
+        print(angle, self.angle, diff)
+        pygame.draw.circle(screen, color="black", center=self.rect.center + self.vector, radius=5)
+        pygame.draw.circle(screen, color="green", center=self.rect.center + self.base_vector, radius=5)
+
+    def __calculate_angle(self):
+        scalar = self.base_vector.x * self.vector.x + self.base_vector.y * self.vector.y
+        angle = math.acos(scalar /
+                          (math.sqrt(self.base_vector.x ** 2 + self.base_vector.y ** 2) * math.sqrt(
+                              self.vector.x ** 2 + self.vector.y ** 2)))* 180 / math.pi
+        return angle
