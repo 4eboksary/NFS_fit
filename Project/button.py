48e86a62867eef3f6a47776c05c1f278fbdb5911
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-02-21 22:00:40 +0200

    Settings_menuCommit

--- Project/button.py
+++ Project/button.py
@@ -6,21 +6,14 @@ class ButtonImage:
         self.width = width
         self.height = height
         self.image = pygame.image.load(image_path) #завантажуємо фото
-
         self.image = pygame.transform.scale(self.image, (width, height)) #підганяємо під наш розмір
-
         self.rect = self.image.get_rect(topleft=(x, y))
-
-        self.is_hovered = False
 
         #малюємо основу для кнопок:
     def draw(self, screen):
         screen.blit(self.image, self.rect.topleft)
 
-    #обробка натискання кнопки
-    def handle_click(self):
-        if self.is_hovered:
-            print("")
-           #Тут будуть виконатися додаткові дії, які повинні відбутися при натисканні кнопки.
-            #Наприклад, відкриття нового вікна тощо.
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos)
+
 
--- Project/menuStart.py
+++ Project/menuStart.py
@@ -10,15 +10,14 @@ pygame.display.set_caption("Car Racer")
 background = pygame.image.load('images/car_background.jpg')
 
 def menu_main():
-
-    # Створюємо кнопки
+    #Створюємо кнопки
     play_button = ButtonImage(174, 170, 252, 74, 'images/button_play.png')
     set_button = ButtonImage(200, 250, 80, 80, 'images/button_settings.png')
     exit_button = ButtonImage(315, 245, 80, 80, 'images/button_exit.png')
 
     run = True
     while run:
-        screen.fill((0, 0, 0)) #заповнюємо чорним кольором
+        screen.fill((0, 0, 0)) #Заповнюємо чорним кольором
         screen.blit(background, (-100, -1))
         menu_font = pygame.font.Font(None, 45)
         text_surf = menu_font.render("CAR VS TIME RACER", True, (0, 0, 255))
@@ -31,16 +30,52 @@ def menu_main():
                 pygame.quit()
                 sys.exit()
 
-        for btn in [play_button, set_button, exit_button]: #по циклу промальовуємо кожну кнопку
+            #Реалізація кнопки вийти
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if exit_button.is_clicked(event.pos):
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+
+            # Реалізація кнопки відкрити налаштування
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if set_button.is_clicked(event.pos):
+                    setting_menu()
+
+        for btn in [play_button, set_button, exit_button]:
             btn.draw(screen)
 
-        pygame.display.flip() #оновлення відображення на екран
+        pygame.display.flip()  #Оновлення відображення на екран
 
 def setting_menu():
-    pass
+    set_background = pygame.image.load('images/set_background.jpg')
+    back_button = ButtonImage(240, 250, 120, 70, 'images/button_back.png')
+    resol_button = ButtonImage(174, 170, 252, 74, 'images/button_resolution.png')
 
+    run = True
+    while run:
+        screen.fill((0, 0, 0))  # Заповнюємо чорним кольором
+        screen.blit(set_background, (-112, -150))
+        menu_font = pygame.font.Font(None, 45)
+        text_surf = menu_font.render("Settings", True, (0, 255, 255))
+        text_rect = text_surf.get_rect(center=(300, 150))
+        screen.blit(text_surf, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back_button.is_clicked(event.pos):
+                    run = False
+
+
+        for btn in [back_button, resol_button]:
+            btn.draw(screen)
+
+        pygame.display.flip()  # Оновлення відображення на екран
 
 if __name__ == "__main__":
     menu_main()
-
-
