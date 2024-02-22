f841c0ff562706bd6912494976a62ffa40b54683
NikitaKrutsenKo <niankr5@gmail.com>
2024-02-22 23:27:46 +0200

    create save_controller template

---
+++ Project/save_controller.py
@@ -0,0 +1,24 @@
+
+class save_controller:
+    def create_save():
+        """
+        creates a new save
+        """
+
+
+    def read_saves():
+        """
+        reads the list of saves
+        """
+
+
+    def choose_save():
+        """choose existing save
+        """
+
+
+    def create_player():
+        """creates player obj
+        """
+
+
