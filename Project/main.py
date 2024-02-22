1cab90c38e6f5091628f50ec0ca397edae967813
NikitaKrutsenKo <niankr5@gmail.com>
2024-02-22 20:35:24 +0200

    change image path, add AWSD

--- Project/main.py
+++ Project/main.py
@@ -22,11 +22,11 @@ def get_car_image(filename, size):
 
 def car_control(keys):
     speed_decay = 1
-    if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-        if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] or keys[pygame.K_DOWN] or keys[pygame.K_w] or keys[pygame.K_s]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             if my_car.speed < my_car.max_speed:
                 my_car.speed += my_car.acceleration
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             if my_car.speed > - my_car.max_speed * 0.5:
                 my_car.speed -= my_car.acceleration * 2
     else:
@@ -35,14 +35,14 @@ def car_control(keys):
         elif my_car.speed < 0:
             my_car.speed += speed_decay
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         my_car.rotate(0.5)
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         my_car.rotate(-0.5)
     my_car.move()
 
 
-my_car_image = get_car_image('Project/images/car2.png', (50, 68))
+my_car_image = get_car_image('images/car2.png', (50, 68))
 my_car = MyCar((400, 300), 270, my_car_image, 1, 130, 10)
 running = True
 while running:
