ae0855196bcd4e70a384c52522a433db48a46958
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-02-27 16:35:49 +0200

    Added_Menu_Video_Settings

--- Project/menu_controller.py
+++ Project/menu_controller.py
@@ -13,14 +13,16 @@ pygame.init()
 # Globals.HEIGHT = 800
 screen = pygame.display.set_mode((Globals.WIDTH, Globals.HEIGHT))
 pygame.display.set_caption("Car Racer")
-background = pygame.image.load('images/car_background.jpg')
 
 # Створюємо кнопки
 play_button = ButtonImage(Globals.WIDTH / 2 - (300 / 2), 300, 300, 130, 'images/button_play.png')
 set_button = ButtonImage(Globals.WIDTH / 2 - (260 / 2), 420, 100, 100, 'images/button_settings.png')
 exit_button = ButtonImage(Globals.WIDTH / 2 + 27, 420, 95, 95, 'images/button_exit.png')
+resol_button = ButtonImage(Globals.WIDTH / 2 - (300 / 2), 250, 300, 120, 'images/button_resolution.png')
+back_button = ButtonImage(Globals.WIDTH / 2 - (200 / 2), 475, 200, 100, 'images/button_back.png')
 
-# menu_main пуеназвав на open_main_menu
+background = pygame.image.load('images/car_background.jpg')
+set_background = pygame.image.load('images/set_background.jpg')
 def open_main_menu():
     run = True
     while run:
@@ -58,14 +60,6 @@ def open_main_menu():
 
 
 def setting_menu():
-    global background, play_button, set_button, exit_button  #Оголошуємо фон і кнопки як глобальну змінну
-
-    set_background = pygame.image.load('images/set_background.jpg')
-    resol_button = ButtonImage(Globals.WIDTH / 2 - (300 / 2), 250, 300, 120, 'images/button_resolution.png')
-    back_button = ButtonImage(Globals.WIDTH / 2 - (194 / 2), 400, 200, 100, 'images/button_back.png')
-    resolution_options = [(800, 600), (1024, 768), (1200, 800)]
-    current_resolution_index = 0
-
     run = True
     while run:
         screen.fill((0, 0, 0))  #Заповнюємо чорним кольором
@@ -81,6 +75,7 @@ def setting_menu():
                 pygame.quit()
                 sys.exit()
 
+            #Реалізація кнопки Back
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if back_button.is_clicked(event.pos):
                     run = False
@@ -88,44 +83,88 @@ def setting_menu():
             # Реалізація кнопки Resolution
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if resol_button.is_clicked(event.pos):
-                    current_resolution_index = (current_resolution_index + 1) % len(resolution_options)
-                    new_resolution = resolution_options[current_resolution_index]  # Отримуємо нове розширення на основі оновленого індексу
-                    Globals.WIDTH, Globals.HEIGHT = new_resolution
-                    pygame.display.set_mode((Globals.WIDTH, Globals.HEIGHT))
-
-                    # Змінюємо фонове зображення відповідно до індексу
-                    if current_resolution_index == 0:
-                        resol_button.set_pos(Globals.WIDTH / 2 - (250 / 2), 250)
-                        back_button.set_pos(Globals.WIDTH / 2 - (150 / 2), 375)
-                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
-                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
-                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
-                        background = pygame.image.load('images/car_background_small.jpg')
-                        set_background = pygame.image.load('images/set_background_small.jpg')
-
-                    elif current_resolution_index == 1:
-                        resol_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 250)
-                        back_button.set_pos(Globals.WIDTH / 2 - (194 / 2), 400)
-                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
-                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
-                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
-                        background = pygame.image.load('images/car_background_medium.jpg')
-                        set_background = pygame.image.load('images/set_background_medium.jpg')
-
-                    elif current_resolution_index == 2:
-                        resol_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 250)
-                        back_button.set_pos(Globals.WIDTH / 2 - (194 / 2), 400)
-                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
-                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
-                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
-                        background = pygame.image.load('images/car_background.jpg')
-                        set_background = pygame.image.load('images/set_background.jpg')
-
-                    screen.blit(background, (0, -80))
+                   video_set_menu()
 
         for btn in [back_button, resol_button]:
             btn.draw(screen)
 
         pygame.display.flip() #Оновлення відображення на екран
 
+def video_set_menu():
+    global background, play_button, set_button, exit_button, resol_button, set_background, back_button  # Оголошуємо фон і кнопки як глобальну змінну
 
+    small_res_button = ButtonImage(Globals.WIDTH / 2 - (325 / 2), 125, 325, 145, 'images/small_resol_button.png')
+    medium_res_button = ButtonImage(Globals.WIDTH / 2 - (295 / 2), 250, 295, 105, 'images/medium_resol_button.png')
+    big_res_button = ButtonImage(Globals.WIDTH / 2 - (280 / 2), 340, 280, 135, 'images/big_resol_button.png')
+    back_button = ButtonImage(Globals.WIDTH / 2 - (200 / 2), 475, 200, 100, 'images/button_back.png')
+
+    run = True
+    while run:
+        screen.fill((0, 0, 0))  # Заповнюємо чорним кольором
+        screen.blit(set_background, (-50, -300))
+        menu_font = pygame.font.Font(None, 70)
+        text_surf = menu_font.render("Video settings", True, (0, 255, 255))
+        text_rect = text_surf.get_rect(center=(Globals.WIDTH / 2, 100))
+        screen.blit(text_surf, text_rect)
+
+        for btn in [back_button, small_res_button, medium_res_button, big_res_button]:
+            btn.draw(screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if back_button.is_clicked(event.pos):
+                        run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if small_res_button.is_clicked(event.pos):
+                        Globals.WIDTH, Globals.HEIGHT = 800, 600
+                        pygame.display.set_mode((Globals.WIDTH, Globals.HEIGHT))
+
+                        resol_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 250)
+                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
+                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
+                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
+                        back_button.set_pos(Globals.WIDTH / 2 - (200 / 2), 200)
+                        small_res_button.set_pos(Globals.WIDTH / 2 - (325 / 2), 250)
+                        medium_res_button.set_pos(Globals.WIDTH / 2 - (295 / 2), 250)
+                        big_res_button.set_pos(Globals.WIDTH / 2 - (280 / 2), 250)
+                        background = pygame.image.load('images/car_background_small.jpg')
+                        set_background = pygame.image.load('images/set_background_small.jpg')
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if medium_res_button.is_clicked(event.pos):
+                        Globals.WIDTH, Globals.HEIGHT = 1024, 768
+                        pygame.display.set_mode((Globals.WIDTH, Globals.HEIGHT))
+                        resol_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 250)
+                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
+                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
+                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
+                        back_button.set_pos(Globals.WIDTH / 2 - (200 / 2), 100)
+                        small_res_button.set_pos(Globals.WIDTH / 2 - (325 / 2), 250)
+                        medium_res_button.set_pos(Globals.WIDTH / 2 - (295 / 2), 250)
+                        big_res_button.set_pos(Globals.WIDTH / 2 - (280 / 2), 250)
+                        background = pygame.image.load('images/car_background_medium.jpg')
+                        set_background = pygame.image.load('images/set_background_medium.jpg')
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if big_res_button.is_clicked(event.pos):
+                        Globals.WIDTH, Globals.HEIGHT = 1200, 800
+                        pygame.display.set_mode((Globals.WIDTH, Globals.HEIGHT))
+                        resol_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 250)
+                        play_button.set_pos(Globals.WIDTH / 2 - (300 / 2), 300)
+                        set_button.set_pos(Globals.WIDTH / 2 - (260 / 2), 420)
+                        exit_button.set_pos(Globals.WIDTH / 2 + 27, 420)
+                        back_button.set_pos(Globals.WIDTH / 2 - (200 / 2), 100)
+                        small_res_button.set_pos(Globals.WIDTH / 2 - (325 / 2), 250)
+                        medium_res_button.set_pos(Globals.WIDTH / 2 - (295 / 2), 250)
+                        big_res_button.set_pos(Globals.WIDTH / 2 - (280 / 2), 250)
+                        background = pygame.image.load('images/car_background.jpg')
+                        set_background = pygame.image.load('images/set_background.jpg')
+
+
+        pygame.display.flip()  # Оновлення відображення на екран
