df514341aa01868d5d5e79540e42560b8eb7e0c1
4eboksary <114410308+4eboksary@users.noreply.github.com>
2024-03-29 00:06:22 +0200

    Test + add_requirements"+"

--- tests/test_check_car_rotation.py
+++ tests/test_check_car_rotation.py
@@ -1,1 +1,13 @@
-# Назар
+import pytest
+from tests import shared_mock_functions
+
+@pytest.mark.parametrize("position, speed, direction, expected_movement",
+                         [((0, 0), 60, 1, True),
+                          ((-70, 50), 60, -1, False)])
+def test_check_car_rotation(position, speed, direction, expected_movement, monkeypatch):
+    monkeypatch.setattr("my_car.get_car_image", shared_mock_functions.mock_get_car_image)
+    test_subject = shared_mock_functions.my_car_mock(position, 5)
+    test_subject.speed = speed * direction
+    test_subject.move()
+
+    assert (test_subject.rect.center[1] - position[1] > 0) == expected_movement
